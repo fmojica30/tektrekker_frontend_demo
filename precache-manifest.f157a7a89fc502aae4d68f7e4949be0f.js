b971c662c5fc2da01242cc88b0b5d786de899918
Fernando Martinez Mojica <fernando.mojica@utexas.edu>
2020-02-27 15:13:39 -0600

    Updates

---
+++ precache-manifest.f157a7a89fc502aae4d68f7e4949be0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e25bd1616953cf794e8f2c4cc61b456f",
+    "url": "/tektrekker_frontend_demo/index.html"
+  },
+  {
+    "revision": "87844f7ecc044d23412b",
+    "url": "/tektrekker_frontend_demo/static/css/2.d063f0c7.chunk.css"
+  },
+  {
+    "revision": "ad6682c387dfac9db1e3",
+    "url": "/tektrekker_frontend_demo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "87844f7ecc044d23412b",
+    "url": "/tektrekker_frontend_demo/static/js/2.bc1253be.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/tektrekker_frontend_demo/static/js/2.bc1253be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad6682c387dfac9db1e3",
+    "url": "/tektrekker_frontend_demo/static/js/main.f1af51cd.chunk.js"
+  },
+  {
+    "revision": "a124e4aad95f200f191f",
+    "url": "/tektrekker_frontend_demo/static/js/runtime-main.77774454.js"
+  }
+]);
